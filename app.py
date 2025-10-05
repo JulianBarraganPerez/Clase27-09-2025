972b62467dc6fbe6ef0774abc0ecb25ba4ba4a2c
JulianBarraganPerez <140422028+JulianBarraganPerez@users.noreply.github.com>
2025-10-05 14:41:23 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def despido():
 # Ejecutar servidor
 # ------------------------------
 if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True, ssl_context=("cert.pem", "key.pem"))
+
